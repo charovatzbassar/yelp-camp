75acee5d0bed825828c252d7a5cc50ca5c5672bb
Bassar Charovatz <81241353+charovatzbassar@users.noreply.github.com>
2023-07-26 16:15:36 +0200

    topology

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const MongoStore = require("connect-mongo")(session);
 //const dbUrl = "mongodb://127.0.0.1:27017/yelp-camp";
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: false,
 });
 
 const db = mongoose.connection;
